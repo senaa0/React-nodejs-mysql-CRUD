c9f90502d4b6bfed3496a80a6bdfbc5f2dbe97d1
senaa0 <senakaradag480@gmail.com>
2023-02-07 17:59:01 +0300

    dosyaları yükledim

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+
+var pool = mysql.createPool({
+
+    host: "93.89.225.112",
+    user: "pehozgun_admina",
+    password: "Admin5050",
+    database: "pehozgun_verita",
+
+});
+
+pool.getConnection(function (err, connection) {
+    // connected! (unless `err` is set)
+});
+
+app.get('/', (req, res) => {
+    res.send("hello sena")
+})
+
+app.post("/create", (req, res) => {
+    const cariadi = req.body.cariadi;
+    const vno = req.body.vno;
+    const vergidairesi = req.body.vergidairesi;
+    const adres = req.body.adres;
+    const telefon = req.body.telefon;
+    const sehir = req.body.sehir;
+    pool.query(
+        "INSERT INTO musteri(cariadi,vno,vergidairesi,adres,telefon,sehir) VALUES(?,?,?,?,?,?)", [cariadi, vno, vergidairesi, adres, telefon, sehir], (err, result) => {
+            if (err) {
+                console.log(err)
+
+            } else {
+                res.send("Values Inserted")
+            }
+        }
+    )
+})
+
+app.get('/all', (req, res) => {
+    pool.query('SELECT * from musteri', (err, rows) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(rows)
+        }
+    });
+})
+
+app.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    pool.query("DELETE FROM musteri WHERE id=?", id, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const cariadi = req.body.cariadi;
+    const vno = req.body.vno;
+    const vergidairesi = req.body.vergidairesi;
+    const adres = req.body.adres;
+    const telefon = req.body.telefon;
+    const sehir = req.body.sehir
+
+    pool.query(
+        "UPDATE musteri SET cariadi = ?, vno = ?,vergidairesi = ?, adres = ?, telefon = ?,sehir = ? WHERE id = ?",
+        [cariadi, vno, vergidairesi, adres, telefon, sehir, id],
+        (err, rows) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(rows)
+            }
+        }
+    )
+})
+
+app.listen(8080, () => {
+    console.log("your server is runnning on port 8080")
+})
